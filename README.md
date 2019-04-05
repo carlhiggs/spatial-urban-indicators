3d238cf4d644ce64cf47ad216f6a5182e002338e
Carl Higgs <carl.higgs@rmit.edu.au>
2019-04-05 13:38:03 +1100

    updated repository docker file and readme

--- README.md
+++ README.md
@@ -8,7 +8,32 @@ This respository contains the processes used in the Bangkok Liveability indicato
 
 * git clone https://carlhiggs@bitbucket.org/carlhiggs/ind_bangkok.git
 
-* follow directions in _environment_setup.sh  (TO DO: flesh these out here)
+* set up analysis environment container, based on OSMnx
+
+```
+cd ./process/docker
+docker build -t ind_bangkok .
+cd ../..
+```
+
+* set up spatial database container, based on Postgis
+
+```
+docker pull mdillon/postgis
+```
+
+
+* run postgis server container
+
+```
+docker run --name=postgis -d -e POSTGRES_USER=postgres -e POSTGRES_PASS=huilhuil!42 -e POSTGRES_DBNAME=ind_bangkok  -p 127.0.0.1:5433:5432 -e pg_data:/var/lib/postgresql mdillon/postgis
+```
+
+* run analysis environment from Bash
+
+```
+docker run --rm -it -u 0 --name ind_bangkok --net=host -v %cd%:/home/bangkok ind_bangkok /bin/bash 
+```
 
 ### Contact ###
 
--- process/_environment_setup.sh
+++ process/_environment_setup.sh
@@ -1,5 +1,7 @@
 # set up analysis environment container, based on OSMnx
-docker pull gboeing/osmnx
+cd ./process/docker
+docker build -t ind_bangkok .
+cd ../..
 
 # set up spatial database container, based on Postgis
 docker pull mdillon/postgis
@@ -8,10 +10,4 @@ docker pull mdillon/postgis
 docker run --name=postgis -d -e POSTGRES_USER=postgres -e POSTGRES_PASS=huilhuil!42 -e POSTGRES_DBNAME=ind_bangkok  -p 127.0.0.1:5433:5432 -e pg_data:/var/lib/postgresql mdillon/postgis
 
 # run analysis environment from Bash
-docker run --rm -it -u 0 --name osmnx --net=host -v %cd%:/home/jovyan/work gboeing/osmnx /bin/bash 
-
-# install additional libraries in analysis environment
-### NOTE more work will be rewd to make these persist; perhaps create own docker image 
-###  - could customise OSMnx docker file
-conda install -y psycopg2
-conda install -y xlrd
+docker run --rm -it -u 0 --name ind_bangkok --net=host -v %cd%:/home/bangkok ind_bangkok /bin/bash 
