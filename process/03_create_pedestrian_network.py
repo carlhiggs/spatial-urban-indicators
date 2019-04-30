0030c0375d952763fe251e68717d9118668f9d4c
Carl Higgs <carl.higgs@rmit.edu.au>
2019-04-30 17:05:42 +1000

    added in installation of java, osmosis, and osm2pgsql to dockerfile (there may be workarounds using OSMnx to get some of the OSM data I require, but not yet aware of these, so trying to adapt what previously worked using osmconvert to new process --- challenges were encountered getting osmconvert to work, hence shift to osmosis

--- process/03_create_pedestrian_network.py
+++ process/03_create_pedestrian_network.py
@@ -18,6 +18,8 @@ from shapely.geometry import shape, MultiPolygon, Polygon
 from sqlalchemy import create_engine
 import geopandas as gpd
 from geoalchemy2 import Geometry, WKTElement
+import requests
+import json
 from script_running_log import script_running_log
 # Import custom variables for National Liveability indicator process
 from _project_setup import *
@@ -46,40 +48,54 @@ pedestrian = (
 # output suffix
 suffix = osm_prefix.strip('osm')
 
-print("Create poly file, using command: "),
-feature = (
- 'PG:"dbname={db} host={host} port={port} user={user} password = {pwd}" {layer}'
- ).format(db  = db, 
-          host= db_host, 
-          port= db_port,
-          user= db_user, 
-          pwd = db_pwd, 
-          layer = 'buffered_study_region_map')
-command = 'python ogr2poly.py {feature} -f Description'.format(feature = feature)
-print('{}'.format(command))
-sp.call(command, shell=True)
-print('Done')
-print('Store poly file in study region folder, using command: ')
-# location of buffered boundary file
-locale_poly = 'poly_10km_study_region_buffer.poly'
-command = 'mv {poly} ../data/study_region/{locale}/{poly}'.format(locale = locale,poly = locale_poly)
-print('\t{}'.format(command))
-sp.call(command, shell=True)
-print("Done.")
+# print("Create poly file, using command: "),
+# locale_poly = 'poly_10km_study_region_buffer.poly'
+# feature = (
+ # 'PG:"dbname={db} host={host} port={port} user={user} password = {pwd}" {layer}'
+ # ).format(db  = db, 
+          # host= db_host, 
+          # port= db_port,
+          # user= db_user, 
+          # pwd = db_pwd, 
+          # layer = 'buffered_study_region_map')
+# command = 'python ogr2poly.py {feature} -f Description'.format(feature = feature)
+# # print('{}'.format(command))
+# sp.call(command, shell=True)
+# command = 'mv {poly} ../data/study_region/{locale}/{poly}'.format(locale = locale,poly = locale_poly)
+# # print('\t{}'.format(command))
+# sp.call(command, shell=True)
+# print("Done.")
 
-print("Extract OSM for studyregion... "),
-osm_region = '../data/study_region/{}/{}'.format(locale,osm_region)
-if os.path.isfile('{}'.format(osm_region)):
-  print('.osm file {} already exists'.format(osm_region))
+# # Extract OSM
+# print("Extract OSM for studyregion... "),
+# osm_region = '../data/study_region/{}/{}'.format(locale,osm_region)
+# if os.path.isfile('{}'.format(osm_region)):
+  # print('.osm file {} already exists'.format(osm_region))
  
-if not os.path.isfile('{}'.format(osm_region)):
-  command = '{osmconvert} {osm} -B={poly} -o={studyregion}'.format(osmconvert = osmconvert, 
-                                                                 osm = osm_data,
-                                                                 poly = locale_poly,
-                                                                 studyregion = osm_region)
-  sp.call(command, shell=True)
-  
-print('Done.')
+# if not os.path.isfile('{}'.format(osm_region)):
+  # command = (
+             # '../../osmosis/bin/osmosis --read-xml file="{}"' 
+                     # '--bounding-polygon file="{}"' 
+                     # '--write-xml file="{}"'
+            # ).format(osm_data,locale_poly,osm_region)
+  # sp.call(command, shell=True)
+# print('Done.')
+
+# # import buffered study region OSM excerpt to pgsql, 
+# print("Copying OSM excerpt to pgsql..."),
+# command = (
+          # 'osm2pgsql -U {user} -l -d {db} --host {host} --port {port}'
+          # '{osm} --hstore --style {style} --prefix {prefix}'
+          # ).format(user   = db_user, 
+                   # db     = db,
+                   # host   = db_host, 
+                   # port   = db_port,
+                   # osm    = osm_source,
+                   # style  = osm2pgsql_style,
+                   # prefix = osm_prefix) 
+# print(command)
+# sp.call(command, shell=True)                           
+# print("Done.")
 
 print('Get networks and save as graphs (retain_all = False*** ie. only main network segment is retained).  Ensure this is appropriate for your study region.  In future, we could make this an optional parameter (false by default, but true if a region is known to have islands.  The problem with network islands is that, in many cases they are artifacts of cartography, not true islands.')
 retain_all = False
@@ -87,44 +103,60 @@ root = '../data/study_region/{locale}'
 filename = '{}_{}'.format(buffered_study_region,year)
 
 if os.path.isfile(os.path.join(root,
-      'osm_{studyregion}_pedestrian{suffix}.graphml'.format(studyregion = filename,
+      'osm_{studyregion}_pedestrian_{suffix}.graphml'.format(studyregion = filename,
                                                             suffix = suffix))):
   print('Pedestrian road network for {} has already been processed; loading this up.'.format(filename))
   W = ox.load_graphml(os.path.join(root,
-      'osm_{studyregion}_pedestrian{suffix}.graphml'.format(studyregion = filename,
+      'osm_{studyregion}_pedestrian_{suffix}.graphml'.format(studyregion = filename,
                                                             suffix = suffix)))
 else:
   subtime = datetime.now()
-  # Extract pedestrian network
-  # c = fiona.open(locale_4326_shp)   
-  # polygon = shape(next(iter(c))['geometry'])
-  polygon = gpd.GeoDataFrame.from_postgis("buffered_study_region_map", engine, geom_col='geom' )['geometry']
+  # load buffered study region in EPSG4326 from postgis
+  polygon =  gpd.GeoDataFrame.from_postgis("buffered_study_region_map", engine, geom_col='geom' )['geom'][0]
   print('Creating and saving all roads network... '),
   W = ox.graph_from_polygon(polygon,  network_type= 'all', retain_all = retain_all)
   ox.save_graphml(W, 
      filename=os.path.join(root,
-                           'osm_{studyregion}_all{suffix}.graphml'.format(studyregion = filename,
+                           'osm_{studyregion}_all_{suffix}.graphml'.format(studyregion = filename,
                            suffix = suffix)), 
      folder=None, 
      gephi=False)
   ox.save_graph_shapefile(W, 
      filename=os.path.join(root,
-                           'osm_{studyregion}_all{suffix}'.format(studyregion = filename,
+                           'osm_{studyregion}_all_{suffix}'.format(studyregion = filename,
                                                                        suffix = suffix)))
   print('Done.')
   print('Creating and saving pedestrian roads network... '),
   W = ox.graph_from_polygon(polygon,  custom_filter= pedestrian, retain_all = retain_all)
   ox.save_graphml(W, filename=os.path.join(root,
-      'osm_{studyregion}_pedestrian{suffix}.graphml'.format(studyregion = filename,
+      'osm_{studyregion}_pedestrian_{suffix}.graphml'.format(studyregion = filename,
                                                             suffix = suffix)), 
       folder=None, 
       gephi=False)
   ox.save_graph_shapefile(W, 
       filename=os.path.join(root,
-      'osm_{studyregion}_pedestrian{suffix}'.format(studyregion = filename,
+      'osm_{studyregion}_pedestrian_{suffix}'.format(studyregion = filename,
                                                     suffix = suffix)))
   print('Done.')                                                            
 
+# Copy network to postgis
+print("Copy the network edges and nodes from gdb to postgis..."),
+command = (
+        ' ogr2ogr -overwrite -progress -f "PostgreSQL" ' 
+        ' PG:"host={host} port=5432 dbname={db}'
+        ' user={user} password = {pwd}" '
+        ' {gdb} {feature} '
+        ' -lco geometry_name="geom"'.format(host = db_host,
+                                     db = db,
+                                     user = db_user,
+                                     pwd = db_pwd,
+                                     gdb = gdb_path,
+                                     feature = '"edges" "nodes"') 
+        )
+print(command)
+sp.call(command, shell=True)
+print("Done (although, if it didn't work you can use the printed command above to do it manually)")  
+  
 # Clean intersections
 print("Prepare cleaned intersections... "),
 
@@ -152,14 +184,7 @@ statement = '''
 conn.execute(statement)      
 print("Done.")
   
-# # Copy joined, cropped Urban Metro meshblock + dwellings feature from postgis to project geodatabase
-# print("Copy intersections to project geodatabase for legacy convenience... "),
-# import arcpy  # This won't work as arcpy is not installed!
-# arcpy.env.workspace = db_sde_path
-# arcpy.CopyFeatures_management('public.{}'.format(intersections_table), 
-                               # os.path.join(gdb_path,intersections_table))  
-# print("Done.")
-# output to completion log    
+
 script_running_log(script, task, start)
 
 # clean up
