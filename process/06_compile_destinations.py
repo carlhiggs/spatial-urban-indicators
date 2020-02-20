f27903b50655bd4488c6dfebc0f597a2766b222b
Carl Higgs <carl.higgs@rmit.edu.au>
2020-02-20 13:34:23 +1100

    re-worked the destination compilation script to import both OSM and non-OSM data sources

--- process/06_compile_destinations.py
+++ process/06_compile_destinations.py
@@ -26,103 +26,131 @@ def main():
     print("Commencing task: {} at {}".format(task,time.strftime("%Y%m%d-%H%M%S")))
     engine = create_engine(connection)
     # get list of datasets used for access analysis
-    df = df_datasets.query("purpose=='destinations'").copy()
+    df = df_datasets.query("purpose=='destinations'").copy().sort_index()
     # define destination type and name from dataset index
     df[['destination','name']] =  df.apply(lambda x: x.name.split(':'),axis=1, result_type="expand")
     # create OSM data flag
     df['osm'] = (df.provider.isin(['OpenStreetMap','OSM'])| df.destination.str.startswith('osm') | df.destination.str.endswith('osm'))
     # retrieve definitions for relevant OSM destinations
     df_osm = df_osm_dest.query('destination in {}'.format(df[df['osm']].name.values)).copy()
-    df_osm['pre-condition'] = df_osm['pre-condition'].replace('NULL','OR')
+    df_osm['condition'] = df_osm['condition'].replace('NULL','OR')
     df_osm_unique = df_osm[['destination','name','domain']].drop_duplicates(subset=['destination'])
+    # The destination schema is used for storing destination features in the study region
+    schema = 'destinations'
+    sql = f'''
+      CREATE SCHEMA IF NOT EXISTS {schema};
+    '''
+    engine.execute(sql)
+    # Ensure any previously created destination tables are purged if existing
+    for row in df.itertuples():
+        destination = getattr(row,'destination')  
+        name = getattr(row,'name')
+        sql = f'''
+            DROP TABLE IF EXISTS {schema}.{destination};
+            DROP TABLE IF EXISTS {schema}.{name};
+            '''
     # Create destination type table and destination in sql database
     # The destination type table is a summary of all destinations in study region
     sql = '''
-      DROP TABLE IF EXISTS dest_type;
-      CREATE TABLE dest_type
+      DROP TABLE IF EXISTS destination_catalog;
+      CREATE TABLE destination_catalog
       (
-       destination varchar PRIMARY KEY,
+       destination varchar,
        name varchar,
-       data varchar,
        domain varchar NOT NULL,
-       count integer
+       count integer,
+       data varchar,
+       PRIMARY KEY (destination, name, data)
       );
        '''
     engine.execute(sql)
-    # The destination schema is used for storing destination features in the study region
-    schema = 'destinations'
-    create_destination_schema = f'''
-      CREATE SCHEMA IF NOT EXISTS {schema};
-      --DROP TABLE IF EXISTS osm_destinations;
-      --CREATE TABLE osm_destinations
-      --(
-      -- dest_oid SERIAL PRIMARY KEY,
-      -- osm_id varchar,
-      -- destination varchar NOT NULL,
-      -- name varchar NOT NULL,
-      -- geom geometry(POINT)
-      --);
-    '''
-    engine.execute(create_destination_schema)
     print("\nImporting destinations...")
-    print("\n{dest:50} {dest_count}".format(dest = "Destination",dest_count = "Import count"))
-    # get bounding box of buffered study region for clipping using ogr2ogr on import
+    print("\n{:25} {:25} {}".format("Destination","Name","Imported"))
     for row in df.itertuples():
         destination = getattr(row,'destination')  
+        sql = f'''
+            CREATE TABLE IF NOT EXISTS {schema}.{destination}
+            (
+            name varchar,
+            oid int,
+            geom geometry(Point, {srid}),
+            PRIMARY KEY (name, oid)
+            );
+        '''
+        engine.execute(sql)
         data = getattr(row,'data_dir')
         name = getattr(row,'name')
         domain = getattr(row,'data_name')
         osm = getattr(row,'osm')
         encoding = getattr(row,'data_encoding')
-        epsg = getattr(row,'epsg')
-        sql = f'''SELECT ST_Extent(ST_Transform(geom,{epsg})) FROM {buffered_study_region};'''
-        urban_region = engine.execute(sql).fetchone()
-        urban_region = [float(x) for x in urban_region[0][4:-1].replace(',',' ').split(' ')]
-        bbox =  '{} {} {} {}'.format(*urban_region)
+        # print(f"{destination} {data} {name} {domain} {osm}")
         if str(encoding) not in ['','nan']:
             encoding = f'--config SHAPE_ENCODING {encoding}'
         else:
             encoding = ''
-        # print(f"{destination} {data} {name} {domain} {osm}")
         if not osm:
+            epsg = int(getattr(row,'epsg'))
+            # transform data if not in project spatial reference
+            if epsg!=srid:
+                transform =   f' -s_srs "ESPG:{epsg}" -t_srs "EPSG:{srid}" '
+            else:
+                transform = ''
+            # get bounding box of buffered study region for clipping external data using ogr2ogr on import
+            sql = f'''SELECT ST_Extent(ST_Transform(geom,{epsg})) FROM {buffered_study_region};'''
+            urban_region = engine.execute(sql).fetchone()
+            urban_region = [float(x) for x in urban_region[0][4:-1].replace(',',' ').split(' ')]
+            bbox =  '{} {} {} {}'.format(*urban_region)
             # do stuff
-            print("\nCopy all pre-processed destinations to postgis..."),
             command = (
-               ' ogr2ogr -overwrite -progress -f "PostgreSQL" ' 
+               ' ogr2ogr -overwrite -f "PostgreSQL" ' 
               f' PG:"host={db_host} port={db_port} dbname={db} user={db_user} password = {db_pwd}" '
               f' {encoding} ".{data}" -lco schema={schema} -nln {name} -clipsrc {bbox} '
                ' -lco geometry_name="geom" -lco OVERWRITE=YES '
-               ' -s_srs ESPG:{epsg} -t_srs EPSG:{srid} '
+              f' {transform}'
                )
-            print(command)
+            # print(command)
             sp.call(command, shell=True)
-            print("Done (although, if it didn't work you can use the printed command above to do it manually)")
-
+            # insert destinations from potentially different sources into combined table
+            sql = f'''
+                ALTER TABLE {schema}.{name} RENAME COLUMN ogc_fid TO oid;
+                INSERT INTO {schema}.{destination} (name, oid, geom)
+                SELECT '{name}',oid, d.geom
+                FROM {schema}.{name} d
+                ON CONFLICT (name,oid) DO NOTHING;
+            '''
+            engine.execute(sql)
+            # catalogue destinations from this data source
+            sql = f'''SELECT count(*) FROM {schema}.{name};'''
+            count = engine.execute(sql).fetchone()[0]
+            sql = f'''
+                INSERT INTO destination_catalog (destination,name,domain,count,data)
+                SELECT '{destination}',
+                       '{name}',
+                       '{domain}',
+                        {count},
+                       '{data}'
+                ON CONFLICT (destination, name, data) DO NOTHING;
+            '''
+            engine.execute(sql)
+            print(f"{destination:25} {name:25} {count:=10d}")
+            sql = f'''
+              CREATE INDEX IF NOT EXISTS {name}_idx ON {schema}.{name} (oid);
+              CREATE INDEX IF NOT EXISTS {name}_gix ON {schema}.{name} USING GIST (geom);
+              '''
+            engine.execute(sql)
         if osm:
-            create_osm_destinations_table = '''
-              DROP TABLE IF EXISTS {};
-              CREATE TABLE {}
-              (
-               dest_oid SERIAL PRIMARY KEY,
-               osm_id varchar,
-               destination varchar NOT NULL,
-               name varchar NOT NULL,
-               geom geometry(POINT)
-              );
-            '''.format(destination)
-            engine.execute(create_osm_destinations_table)
             dest_condition = []
             for condition in ['AND','OR','NOT']:
-            # for condition in df_osm_dest[df_osm_dest['destination']==dest]['pre-condition'].unique():
+            # for condition in df_osm_dest[df_osm_dest['destination']==destination]['condition'].unique():
                 # print(condition)
                 if condition == 'AND':
-                    clause = ' AND '.join(df_osm_dest[(df_osm_dest['destination']==dest)&(df_osm_dest['pre-condition']=='AND')].apply(lambda x: "{} IS NOT NULL".format(x.key) if x.value=='NULL' else "{} = '{}'".format(x.key,x.value),axis=1).values.tolist())
+                    clause = ' AND '.join(df_osm[(df_osm['destination']==name)&(df_osm['condition']=='AND')].apply(lambda x: "{} IS NOT NULL".format(x.key) if x.value=='NULL' else "{} = '{}'".format(x.key,x.value),axis=1).values.tolist())
                     dest_condition.append(clause)
                 if condition == 'OR':
-                    clause = ' OR '.join(df_osm_dest[(df_osm_dest['destination']==dest)&(df_osm_dest['pre-condition']=='OR')].apply(lambda x: "{} IS NOT NULL".format(x.key) if x.value=='NULL' else "{} = '{}'".format(x.key,x.value),axis=1).values.tolist())
+                    clause = ' OR '.join(df_osm[(df_osm['destination']==name)&(df_osm['condition']=='OR')].apply(lambda x: "{} IS NOT NULL".format(x.key) if x.value=='NULL' else "{} = '{}'".format(x.key,x.value),axis=1).values.tolist())
                     dest_condition.append(clause)
                 if condition != 'NOT':
-                    clause = ' AND '.join(df_osm_dest[(df_osm_dest['destination']==dest)&(df_osm_dest['pre-condition']=='NOT')].apply(lambda x: "{} IS NOT NULL".format(x.key) if x.value=='NULL' else "{} != '{}' OR access IS NULL".format(x.key,x.value),axis=1).values.tolist())
+                    clause = ' AND '.join(df_osm[(df_osm['destination']==name)&(df_osm['condition']=='NOT')].apply(lambda x: "{} IS NOT NULL".format(x.key) if x.value=='NULL' else "{} != '{}' OR access IS NULL".format(x.key,x.value),axis=1).values.tolist())
                     dest_condition.append(clause)
             dest_condition = [x for x in dest_condition if x!='']
             # print(len(dest_condition))
@@ -130,58 +158,68 @@ def main():
                 dest_condition = dest_condition[0]
             else:
                 dest_condition = '({})'.format(') AND ('.join(dest_condition))
-            print(dest_condition)
-            combine__point_destinations = f'''
-              INSERT INTO osm_destinations (osm_id, destination,name,geom)
-              SELECT osm_id, '{dest}','{name}', d.geom 
-                FROM {osm_prefix}_point d
-               WHERE {dest_condition};
+            # print(dest_condition)
+            sql = f'''
+              DROP TABLE IF EXISTS {schema}.{name};
+              CREATE TABLE {schema}.{name} AS
+              SELECT (row_number() over())::int oid,
+                     osm_id,
+                     "data",
+                     geom
+              FROM
+              (SELECT * FROM
+                (SELECT osm_id,
+                        '{osm_prefix}_point' AS data,
+                        geom
+                   FROM {osm_prefix}_point d WHERE {dest_condition}) point
+                 UNION
+                (SELECT osm_id,
+                        '{osm_prefix}_polygon' AS data,
+                        ST_Centroid(d.geom)
+                   FROM {osm_prefix}_polygon d WHERE {dest_condition})
+               ORDER BY data, osm_id) t
+               ;
             '''
-            curs.execute(combine__point_destinations)
-            conn.commit()        
-        
-        # get point dest count in order to set correct auto-increment start value for polygon dest OIDs
-        curs.execute(f'''SELECT count(*) FROM osm_destinations WHERE destination = '{dest}';''')
-        dest_count = int(list(curs)[0][0])       
-        
-        combine_poly_destinations = f'''
-          INSERT INTO osm_destinations (osm_id, destination,name,geom)
-          SELECT osm_id, '{dest}','{name}', ST_Centroid(d.geom)
-            FROM {osm_prefix}_polygon d
-           WHERE {dest_condition};
-        '''
-        curs.execute(combine_poly_destinations)
-        conn.commit()      
-        
-        curs.execute(f'''SELECT count(*) FROM osm_destinations WHERE destination = '{dest}';''')
-        dest_count = int(list(curs)[0][0])  
-        
-        if dest_count > 0:
-          summarise_dest_type = f'''
-          INSERT INTO dest_type (destination,name,domain,count)
-          SELECT '{dest}',
-                 '{name}',
-                 '{domain}',
-                 {dest_count}
+            # print(sql)
+            engine.execute(sql)
+            # insert destinations from potentially different sources into combined table
+            sql = f'''
+                INSERT INTO {schema}.{destination} (name, oid, geom)
+                SELECT '{name}',oid, d.geom
+                FROM {schema}.{name} d
+                ON CONFLICT (name,oid) DO NOTHING;
+            '''
+            engine.execute(sql)
+            # catalogue destinations from this data source
+            sql = f'''SELECT count(*) FROM {schema}.{name};'''
+            count = engine.execute(sql).fetchone()[0]
+            sql = f'''
+                INSERT INTO destination_catalog (destination,name,domain,count,data)
+                SELECT '{destination}' AS destination,
+                       '{name}'        AS name,
+                       '{domain}'      AS domain,
+                        {count}        AS count,
+                        data || $$:{dest_condition}$$ AS data
+                FROM {schema}.{name}
+                GROUP BY destination, name, data, domain
+                ON CONFLICT (destination, name, data) DO NOTHING;
+            '''
+            engine.execute(sql)
+            print(f"{destination:25} {name:25} {count:=10d}")
+            sql = f'''
+              CREATE INDEX IF NOT EXISTS {name}_idx ON {schema}.{name} (oid);
+              CREATE INDEX IF NOT EXISTS {name}_gix ON {schema}.{name} USING GIST (geom);
+              '''
+            engine.execute(sql)
+        sql = f'''
+          CREATE INDEX IF NOT EXISTS {destination}_idx ON {schema}.{destination} (oid);
+          CREATE INDEX IF NOT EXISTS {destination}_gix ON {schema}.{destination} USING GIST (geom);
           '''
-          curs.execute(summarise_dest_type)
-          conn.commit()
-          # print destination name and tally which have been imported
-          print(f"\n{dest:50} {dest_count:=10d}")
-          print(f"({dest_condition})")
-
-    create_osm_destinations_indices = '''
-      CREATE INDEX osm_destinations_destination_idx ON osm_destinations (destination);
-      CREATE INDEX osm_destinations_geom_geom_idx ON osm_destinations USING GIST (geom);
-    '''
-    curs.execute(create_osm_destinations_indices)
-    conn.commit()
-    curs.execute(grant_query)
-
+        engine.execute(sql)
+    engine.execute(grant_query)
     # output to completion log    
     script_running_log(script, task, start, locale)
-    conn.close()
-
+    engine.dispose()
         
 if __name__ == '__main__':
     main()
