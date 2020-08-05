671cac7a2efc40812086b4c8a9c0ea1f2532b541
Carl Higgs <carl.higgs@rmit.edu.au>
2020-08-05 15:32:38 +1000

    added in create accessibility indicators script

--- process/_create_accessibility_indicators.py
+++ process/_create_accessibility_indicators.py
@@ -21,6 +21,8 @@ from script_running_log import script_running_log
 
 # Import custom variables for National Liveability indicator process
 from _project_setup import *
+
+ 
  
 def main():
     # simple timer for log file
@@ -32,30 +34,72 @@ def main():
     gpkg_path = os.path.join(locale_maps,'gpkg')
     engine_sqlite = create_engine(f'sqlite:///{gpkg_path}/{study_region}.gpkg',module = sqlite3)
                       
+    if 'reprocess' in sys.argv:
+        reprocess = True
+    else:
+        reprocess = False
+                      
     # retrieve subset of datasets which are files to be joined based on accessibility analyses (previously run)
     df = df_datasets.query("purpose=='indicators' & type=='access'").copy().sort_index()
     
     # retrieve list of additional covariate names from population data set
-    population = pandas.read_csv(population_linkage[analysis_scale]['data'],
-                                 index_col=population_linkage[analysis_scale]['linkage']) 
-    data_fields = get_df_data_fields(population,population_map_fields)
+    population = pandas.read_csv(population_linkage[analysis_scale]['data'],index_col=population_linkage[analysis_scale]['linkage']) 
+    population_fields = get_df_data_fields(population,population_map_fields)
     
+    # create dictionary of additional covariate names for mapping (with superscript 2)
+    column_names = {}
+    # format to display superscript 2 for square kilometres
+    for f in population_fields:
+        column_names[f] = f.replace('sqkm','km\u00B2')
+ 
     schema = 'ind_area'
-    measure = 'percent_access'
     for area in df.linkage_layer.unique():
         area_id = areas[area]['id']
-        area_attribution = areas[area]['attribution']
+        # note: for aggregation to areas to be successful, an area linkage layer must be defined
         print(area)
         for row in df.query(f"linkage_layer=='{area}'").itertuples():
-            map_name_suffix = row.table_out_name
+            destination = getattr(row,'destination')  
+            print(f"  - {destination}")
+            distance = getattr(row,'resolution')
+            measure = 'percent_access'
+            
+            area_layer = getattr(row,'linkage_layer')
+            area_linkage_id = getattr(row,'linkage_id')
+            map_name_suffix = getattr(row,'table_out_name')
             table = map_name_suffix.replace('_pop_pct','')
+            # get information about this measure
+            data_type = valid_type(getattr(row,'data_type'))
+            description = getattr(row,'alias')
+            heading = '{}: {}'.format(full_locale,getattr(row,'map_heading'))
+            aggregation = getattr(row,'aggregation')
+            linkage_id = getattr(row,'linkage_id')
+            point_overlay_xy = getattr(row,'point_overlay_xy')
+            display_id = area_layer
+            map_field = getattr(row,'map_field')
+            units = getattr(row,'units')
+            rate = getattr(row,'rate')
+            rate_units = getattr(row,'rate_units')
+            rate_scale = getattr(row,'rate_scale')
+            units = format_units(units,rate_units,rate_scale)
+            target_year = getattr(row,'year_target')
+            source = getattr(row,'provider')
+            potential_column_width = len(f'{map_field}_{aggregation}')
+            if potential_column_width < pd.get_option("display.max_colwidth"):
+                pd.set_option("display.max_colwidth", potential_column_width)
             print('\t{}'.format(table))
-            if 'skip_tables' not in sys.argv:
-                generate_isid_csv_template(engine, row, outpath=f'{locale_maps}/csv', measure, schema, prefix=study_region)
-            if 'skip_maps' not in sys.argv:
-                # Create map
-                generate_map(engine,row,out_path=locale_maps,data_fields=data_fields,prefix=locale,schema=schema,map_attribution=map_attribution,area_attribution=area_attribution,map_style_html_css=map_style,table=table)
-                
+            map_name = f'{locale}_ind_{map_name_suffix}'
+            if os.path.isfile(f'{locale_maps}/html/{map_name}.html') and reprocess==False:
+                print('\t - File appears to already have been processed (HTML output exists); skipping.')
+            else:
+                if 'skip_tables' not in sys.argv:
+                    csv_file = f'{locale_maps}/csv/{study_region}_{map_name_suffix}.csv'
+                    generate_isid_csv_template(engine, area_layer, area_linkage_id, target_year, measure, units, schema, table, csv_file)
+                if 'skip_maps' not in sys.argv:
+                    # Create map
+                    attribution = '{} | {} | {} data: {}'.format(map_attribution,areas[area_layer]['attribution'],map_field,source)
+                    coalesce_na = '{}'.format(getattr(row,'coalesce_na'))
+                    data_fields = population_fields
+                    generate_map(engine,map_name,map_attribution, attribution, area, heading, measure, map_field,  linkage_id, schema, table, coalesce_na, data_fields,column_names,map_style,outpath=locale_maps)
     # output to completion log                  
     script_running_log(script, task, start, locale)
     engine.dispose()
