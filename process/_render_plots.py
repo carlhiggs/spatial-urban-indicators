3503cd9d69eed844016c1c966695dad3b0937b55
Carl Higgs <carlhiggs@gmail.com>
2020-10-20 15:01:12 +1100

    updated the render plots code

--- process/_render_plots.py
+++ process/_render_plots.py
@@ -52,127 +52,128 @@ def main():
         row_type = df.loc[row,'type']
         data_type = valid_type(df.loc[row,'data_type'])
         description = df.loc[row,'indicator_measure']
-        print(f"\n - {description}")
         description = '\n'.join(wrap(description, 80))
         plot_data_y = df.loc[row,'table_out_name'].replace(' ','_',).replace('-','_')
         area_layer = df.loc[row,'linkage_layer']
-        area_linkage_id = df.loc[row,'linkage_id']
-        aggregation = df.loc[row,'aggregation']
-        linkage_id = df.loc[row,'linkage_id']
-        point_overlay_xy = df.loc[row,'point_overlay_xy']
-        display_id = area_layer
-        map_field = df.loc[row,'map_field']
-        rate = df.loc[row,'rate']
-        rate_units = df.loc[row,'rate_units']
-        rate_scale = df.loc[row,'rate_scale']
-        if aggregation not in ['','nan']:
-            potential_column_width = len(map_field) 
-        else:
-            potential_column_width = len(map_field) + len(aggregation) + 1
-        if potential_column_width < pd.get_option("display.max_colwidth"):
-            pd.set_option("display.max_colwidth", potential_column_width)
-        map_name = f'{locale}_ind_{plot_data_y}'
-        # Prepare plot
-        y    = plot_data_y
-        ylab = description
-        title = description
-        x1   = 'population'     
-        x2   = 'population per sqkm'
-        if row_type!='access':
-            sql = f'''
-            SELECT a.{area_layer},
-                   a.{area_layer}_en,
-                   a.{area_layer}_th,
-                   a."{x1}",
-                   a."{x2}",
-                   d.{y}
-            FROM {area_layer} a
-            LEFT JOIN {y} d USING ({linkage_id});
-            '''
-        else:
-            y = plot_data_y
-            schema='ind_area'
-            table = plot_data_y.replace('_pop_pct','')
-            table_y = 'percent_access'
-            sql = f'''
-            SELECT a.{area_layer}_id,
-                   a.{area_layer}_en,
-                   a.{area_layer}_th,
-                   a."{x1}",
-                   a."{x2}",
-                   d.{table_y} {y}
-            FROM {area_layer} a
-            LEFT JOIN {schema}.{table} d USING ({linkage_id});
-            '''
-        
-        data = pd.read_sql(sql,engine)
-        data['regions_of_interest'] = 'Other regions'
-        data.loc[~(data[regions_of_interest_variable].isin(regions_of_interest)),'regions_of_interest'] = 'Case studies'
-        data['label'] = data[f'{area_layer}_th'] + ' ' + data[f'{area_layer}_en']
-        data['full_label'] = data['label'] 
-        data.loc[~(data[regions_of_interest_variable].isin(regions_of_interest)),'label'] = ''
-        
-        #colours = ['#d01c8b','#f1b6da','#f7f7f7','#b8e186','#4dac26']
-        #colour_binary = [colours[0], colours[-1]]
-        
-        for dir in ['pdf','svg']:
-            folder_dir = (f'../output/{study_region}/{dir}/plots/')
-            if not os.path.exists(folder_dir):
-                os.makedirs(folder_dir)
-        
-        if area_layer == regions_of_interest_scale:
-            # scatterplots
-            # by population
-            location = f'../output/{study_region}/pdf/plots/{y}_{x1}.pdf'.replace(' ','_')
-            if not os.path.exists(location):
-                font = {'family':'Garuda','size':9.0}
-                matplotlib.rc('font', **font)
-                g = sns.lmplot(x1, y, data=data, hue='regions_of_interest', fit_reg=False)
-                g._legend.remove()
-                g.set(xlabel=x1.capitalize(), ylabel=title)
-                label_point(data[x1], data[y], data['label'], plt.gca())
-                for ax in g.axes[:,0]:
-                    ax.get_xaxis().set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, p: f"{x:,.0f}"))
-                    if data_type in ['integer','Int64']:
-                        ax.get_yaxis().set_major_locator(matplotlib.ticker.MaxNLocator(integer=True))
-                        ax.get_yaxis().set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, p: f"{x:,.0f}"))
-                    else:
-                        ax.get_yaxis().set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, p: "{:,.9g}".format(round(x,3))))
-                
-                plt.tight_layout()
-                # plt.show()   
-                g.savefig(location)   
-                plt.close()
-                print(f"\t{location} saved.")
+        if df.loc[row,'plot'] == area_layer:
+            print(f"\n - {description}")
+            area_linkage_id = df.loc[row,'linkage_id']
+            aggregation = df.loc[row,'aggregation']
+            linkage_id = df.loc[row,'linkage_id']
+            point_overlay_xy = df.loc[row,'point_overlay_xy']
+            display_id = area_layer
+            map_field = df.loc[row,'map_field']
+            rate = df.loc[row,'rate']
+            rate_units = df.loc[row,'rate_units']
+            rate_scale = df.loc[row,'rate_scale']
+            if aggregation not in ['','nan']:
+                potential_column_width = len(map_field) 
             else:
-                print(f"\t{location} already exists.")
-            
-            # by population per square kilometre
-            location = f'../output/{study_region}/pdf/plots/{y}_{x2}.pdf'.replace(' ','_')
-            if not os.path.exists(location):
-                font = {'family':'Garuda','size':9.0}
-                matplotlib.rc('font', **font)
-                g = sns.lmplot(x2, y, data=data, hue='regions_of_interest', fit_reg=False)
-                g._legend.remove()
-                g.set(xlabel=x2.replace('sqkm','km\u00B2').capitalize(), ylabel=title)
-                label_point(data[x2], data[y], data['label'], plt.gca())
-                for ax in g.axes[:,0]:
-                    ax.get_xaxis().set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, p: f"{x:,.0f}"))
-                    if data_type in ['integer','Int64']:
-                        ax.get_yaxis().set_major_locator(matplotlib.ticker.MaxNLocator(integer=True))
-                        ax.get_yaxis().set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, p: f"{x:,.0f}"))
-                    else:
-                        ax.get_yaxis().set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, p: "{:,.9g}".format(round(x,3))))
-                
-                plt.tight_layout()
-                # plt.show()
-                
-                g.savefig(location)   
-                plt.close()
-                print(f"\t{location} saved.")
+                potential_column_width = len(map_field) + len(aggregation) + 1
+            if potential_column_width < pd.get_option("display.max_colwidth"):
+                pd.set_option("display.max_colwidth", potential_column_width)
+            map_name = f'{locale}_ind_{plot_data_y}'
+            # Prepare plot
+            y    = plot_data_y
+            ylab = description
+            title = description
+            x1   = 'population'     
+            x2   = 'population per sqkm'
+            if row_type!='access':
+                sql = f'''
+                SELECT a.{area_layer},
+                       a.{area_layer}_en,
+                       a.{area_layer}_th,
+                       a."{x1}",
+                       a."{x2}",
+                       d.{y}
+                FROM {area_layer} a
+                LEFT JOIN {y} d USING ({linkage_id});
+                '''
             else:
-                print(f"\t{location} exists.")
+                y = plot_data_y
+                schema='ind_area'
+                table = plot_data_y.replace('_pop_pct','')
+                table_y = 'percent_access'
+                sql = f'''
+                SELECT a.{area_layer}_id,
+                       a.{area_layer}_en,
+                       a.{area_layer}_th,
+                       a."{x1}",
+                       a."{x2}",
+                       d.{table_y} {y}
+                FROM {area_layer} a
+                LEFT JOIN {schema}.{table} d USING ({linkage_id});
+                '''
             
+            data = pd.read_sql(sql,engine)
+            data['regions_of_interest'] = 'Other regions'
+            data.loc[~(data[regions_of_interest_variable].isin(regions_of_interest)),'regions_of_interest'] = 'Case studies'
+            data['label'] = data[f'{area_layer}_th'] + ' ' + data[f'{area_layer}_en']
+            data['full_label'] = data['label'] 
+            data.loc[~(data[regions_of_interest_variable].isin(regions_of_interest)),'label'] = ''
+            
+            #colours = ['#d01c8b','#f1b6da','#f7f7f7','#b8e186','#4dac26']
+            #colour_binary = [colours[0], colours[-1]]
+            
+            for dir in ['pdf','svg']:
+                folder_dir = (f'../output/{study_region}/{dir}/plots/')
+                if not os.path.exists(folder_dir):
+                    os.makedirs(folder_dir)
+            
+            if area_layer == regions_of_interest_scale:
+                # scatterplots
+                # by population
+                location = f'../output/{study_region}/pdf/plots/{y}_{x1}.pdf'.replace(' ','_')
+                if not os.path.exists(location):
+                    font = {'family':'Garuda','size':9.0}
+                    matplotlib.rc('font', **font)
+                    g = sns.lmplot(x = x1,y = y, data=data, hue='regions_of_interest', fit_reg=False)
+                    g._legend.remove()
+                    g.set(xlabel=x1.capitalize(), ylabel=title)
+                    label_point(data[x1], data[y], data['label'], plt.gca())
+                    for ax in g.axes[:,0]:
+                        ax.get_xaxis().set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, p: f"{x:,.0f}"))
+                        if data_type in ['integer','Int64']:
+                            ax.get_yaxis().set_major_locator(matplotlib.ticker.MaxNLocator(integer=True))
+                            ax.get_yaxis().set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, p: f"{x:,.0f}"))
+                        else:
+                            ax.get_yaxis().set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, p: "{:,.9g}".format(round(x,3))))
+                    
+                    plt.tight_layout()
+                    # plt.show()   
+                    g.savefig(location)   
+                    plt.close()
+                    print(f"\t{location} saved.")
+                else:
+                    print(f"\t{location} already exists.")
+                
+                # by population per square kilometre
+                location = f'../output/{study_region}/pdf/plots/{y}_{x2}.pdf'.replace(' ','_')
+                if not os.path.exists(location):
+                    font = {'family':'Garuda','size':9.0}
+                    matplotlib.rc('font', **font)
+                    g = sns.lmplot(x = x2,y = y, data=data, hue='regions_of_interest', fit_reg=False)
+                    g._legend.remove()
+                    g.set(xlabel=x2.replace('sqkm','km\u00B2').capitalize(), ylabel=title)
+                    label_point(data[x2], data[y], data['label'], plt.gca())
+                    for ax in g.axes[:,0]:
+                        ax.get_xaxis().set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, p: f"{x:,.0f}"))
+                        if data_type in ['integer','Int64']:
+                            ax.get_yaxis().set_major_locator(matplotlib.ticker.MaxNLocator(integer=True))
+                            ax.get_yaxis().set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, p: f"{x:,.0f}"))
+                        else:
+                            ax.get_yaxis().set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, p: "{:,.9g}".format(round(x,3))))
+                    
+                    plt.tight_layout()
+                    # plt.show()
+                    
+                    g.savefig(location)   
+                    plt.close()
+                    print(f"\t{location} saved.")
+                else:
+                    print(f"\t{location} exists.")
+                
             # Horizontal bar plot
             location = f'../output/{study_region}/pdf/plots/{y}.pdf'.replace(' ','_')
             if not os.path.exists(location):
@@ -180,7 +181,7 @@ def main():
                 matplotlib.rc('font', **font)
                 pd_data = data.sort_values(y)
                 plt.figure(figsize=(14,10))
-                ax = sns.barplot(pd_data[y], pd_data['full_label'])
+                ax = sns.barplot(x = pd_data[y],y = pd_data['full_label'])
                 ax.set(xlabel=title, ylabel=area_layer.title())
                 if data_type in ['integer','Int64']:
                     ax.xaxis.set_major_locator(matplotlib.ticker.MaxNLocator(integer=True))
